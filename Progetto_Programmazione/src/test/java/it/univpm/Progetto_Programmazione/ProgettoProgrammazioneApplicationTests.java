600a630b6e76c0a8b76e364b9a66dd4f3b4be5a6
User <User@DESKTOP-GPK27R3>
2019-11-26 15:36:43 +0100

    primo commit

---
+++ Progetto_Programmazione/src/test/java/it/univpm/Progetto_Programmazione/ProgettoProgrammazioneApplicationTests.java
@@ -0,0 +1,16 @@
+package it.univpm.Progetto_Programmazione;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProgettoProgrammazioneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
