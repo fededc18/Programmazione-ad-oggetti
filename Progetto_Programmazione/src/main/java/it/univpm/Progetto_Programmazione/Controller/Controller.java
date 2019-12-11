c2aa37f11668ac601aed2fff11c302f9896ef342
fededc18 <53344251+fededc18@users.noreply.github.com>
2019-12-11 17:54:43 +0100

    controller

--- Progetto_Programmazione/src/main/java/it/univpm/Progetto_Programmazione/Controller/Controller.java
+++ Progetto_Programmazione/src/main/java/it/univpm/Progetto_Programmazione/Controller/Controller.java
@@ -12,13 +12,58 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import progetto.GestioneDati;
-import progetto.Tool.*;
-import progetto.GestioneDati.EuropeanUn;
-import progetto.Service.EuropeanService;
-import progetto.Service.*;
+import it.univpm.Dati_Europa.Model.MainCat;
+import it.univpm.Dati_Europa.Services.Services;
+import it.univpm.Progetto_Programmazione.GestioneDati.*;
+import it.univpm.Progetto_Programmazione.GestioneDati.EuropeanUn;
+import it.univpm.Progetto_Programmazione.Model.MacroCat;
+import it.univpm.Progetto_Programmazione.Model.SubCat;
+import it.univpm.Progetto_Programmazione.Service.*;
+import it.univpm.Progetto_Programmazione.Service.Service;
 
+/*
+ * Classe concepita per gestire le richieste effetuate dall'utente
+ */
 
 public class Controller {
+	
+	@Autowired
+	private Service servizio;
+	
+	/*
+	 * Costruttore della classe
+	 * Grazie a @Autowired viene lanciato automaticamnete all'avvio i String 
+	 * ed esegue il collegamento con Service
+	 */
+	
+	public Controller(Service servizio)
+	{
+		this.servizio = servizio;
+	}
+	
+	/**
+	 * Metodi per la comunicazione con il client che gestiscono le richieste get e post
+	*/
+	/**
+	 * Metodo che gestisce la richiesta GET alla rotta "/Data" con lo scopo di restituire tutto il tsv
+	 * @return la lista di tutti gli elementi del tsv
+	*/
+	
+	@GetMapping("/Data")
+	public List getAllData()
+	{
+		return servizio.getData();
+	}
+	
+	 /**
+     * Metodo che gestisce la richiesta GET alla rotta "/Metadata", restituisce i metadata
+     * @return lista dei metadata
+     */
+    @GetMapping("/Metadata")
+    public List<Map> getMetadata() {
+        return service.getMetadata();
+    }
+	
+	
 
 }
