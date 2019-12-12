e9d80adfda59f3c1e543c18f62a2cc06afb7b171
sofialahiane <sofialahiane@192.168.9.100>
2019-12-12 14:58:29 +0100

    aggiornamento classe energy

--- Progetto_Programmazione/src/main/java/it/univpm/Progetto_Programmazione/GestioneDati/EuropeanUn.java
+++ Progetto_Programmazione/src/main/java/it/univpm/Progetto_Programmazione/GestioneDati/EuropeanUn.java
@@ -2,82 +2,37 @@ package it.univpm.Progetto_Programmazione.GestioneDati;
 
 import java.io.Serializable;
 
-public class Energia implements Serializable 
+public class Energy implements Serializable 
 {
 
-  private String nrg_bal,siec,unit,geo\time;
-  private int [] Valori = new int [] 
+  private String Nrg_bal,Siec,Unit,Geo\time;
+  private int [] Valori = new int [12];
+}
   
- 
+  /**
+   * Costruttore della classe
+   * @param nrg_bal
+   * @param siec
+   * @param unit
+   * @param geo\time
+   * @param valori
+   */
   
-  public EuropeanUn (int period,String fc_en,String fc_ind_en,String fc_oth_en,String members_UE,String unit_measure, double value)
+  
+  public Energy (String  nrg_bal, String siec, String unit, String geo\time, int valori[] )
   {
-	  this.Fc_en= fc_en;
-	  this.Fc_ind_en= fc_ind_en;
-	  this.Fc_oth_en= fc_oth_en;
-	  this.Members_UE= members_UE;
-	  this.Period= period;
-	  this.Unit_measure= unit_measure;
-	  this.Value= value;
+	  this.Nrg_bal= nrg_bal;
+	  this.Siec= siec;
+	  this.Unit= unit;
+	  this.Geo\time= geo\time;
+	  this.Valori= valori;
+	 
   }
   
 //Implementazione Getters and Setters
+  
 
-  public int getPeriod() {
-	return Period;
-  }
 
-  public void setPeriod(int period) {
-	Period = period;
-  }
-
-  public String getFc_en() {
-	return Fc_en;
-  }
-
-  public void setFc_en(String fc_en) {
-	Fc_en = fc_en;
-  }
-
-  public String getFc_ind_en() {
-	return Fc_ind_en;
-  }
-
-  public void setFc_ind_en(String fc_ind_en) {
-	Fc_ind_en = fc_ind_en;
-  }
-
-  public String getFc_oth_en() {
-	return Fc_oth_en;
-  }
-
-  public void setFc_oth_en(String fc_oth_en) {
-	Fc_oth_en = fc_oth_en;
-  }
-
-  public String getMembres_UE() {
-	return Membres_UE;
-  }
-
-  public void setMembres_UE(String membres_UE) {
-	Membres_UE = membres_UE;
-  }
-
-  public String getUnit_measure() {
-	return Unit_measure;
-  }
-
-  public void setUnit_measure(String unit_measure) {
-	Unit_measure = unit_measure;
-  }
-
-  public double getValue() {
-	return Value;
-  }
-
-  public void setValue(double value) {
-	Value = value;
-  }
 
   /*utilizzo il metodo toString per la stampa dell'oggetto
    * 
@@ -85,8 +40,9 @@ public class Energia implements Serializable
    */
   @Override
   public String toString() {
-	  return "EuropeanUE [Members_UE=" + Members_UE + "Period=" + Period + ",Fc_en="
+	  return "Energy[Members_UE=" + Members_UE + "Period=" + Period + ",Fc_en="
 				+ Fc_en + ",Fc_ind_en=" + Fc_ind_en + "Fc_oth_en" + Fc_oth_en + ",unit_measure=" 
 			    + Unit_measure + ",value=" + Value + " ]";
+	
   }
 }
